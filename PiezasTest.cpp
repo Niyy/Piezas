4235e5d9efca25b3dd8f557e3d17b7f286d7159e
Austin Meyer <meyerforge@gmail.com>
2019-12-11 18:26:21 -0800

    Put in some basic tests.

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -16,5 +16,106 @@ class PiezasTest : public ::testing::Test
 
 TEST(PiezasTest, sanityCheck)
 {
+	Piezas new_game;
+
 	ASSERT_TRUE(true);
+}
+
+
+
+TEST(PiezasTest, checkEmpty)
+{
+	Piezas new_game;
+
+	ASSERT_EQ(new_game.pieceAt(0, 0), Blank);
+}
+
+
+
+
+TEST(PiezasTest, placeFirstPiece)
+{
+	Piezas new_game;
+
+	ASSERT_EQ(new_game.dropPiece(0), X);
+}
+
+
+
+TEST(PiezasTest, checkTurnChange)
+{
+	Piezas new_game;
+
+	new_game.dropPiece(0);
+
+	ASSERT_EQ(new_game.dropPiece(0), O);
+}
+
+
+
+
+TEST(PiezasTest, checkFullColumn)
+{
+	Piezas new_game;
+
+	new_game.dropPiece(0)
+	new_game.dropPiece(0)
+	new_game.dropPiece(0)
+
+	ASSERT_EQ(new_game.dropPiece(0), Blank);
+}
+
+
+
+
+TEST(PiezasTest, checkPiecePlaced)
+{
+	Piezas new_game;
+
+	new_game.dropPiece(0)
+
+	ASSERT_EQ(new_game.pieceAt(2, 0), X);
+}
+
+
+
+
+TEST(PiezasTest, checkPieceplaceSecond)
+{
+	Piezas new_game;
+
+	new_game.dropPiece(0)
+	new_game.dropPiece(0)
+
+	ASSERT_EQ(new_game.pieceAt(2, 0), X);
+}
+
+
+
+
+TEST(PiezasTest, checkOutOfBoundsRow)
+{
+	Piezas new_game;
+
+	ASSERT_EQ(new_game.pieceAt(-1, 0), X);
+}
+
+
+
+
+TEST(PiezasTest, checkOutOfBoundsColumn)
+{
+	Piezas new_game;
+
+	ASSERT_EQ(new_game.pieceAt(0, -1), X);
+}
+
+
+
+
+TEST(PiezasTest, checkOutOfBoundsBoth)
+{
+	Piezas new_game;
+
+	ASSERT_EQ(new_game.pieceAt(-1, -1), X);
 }
